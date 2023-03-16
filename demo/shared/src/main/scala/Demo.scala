1c54f91415847e53b7432c68ab441d8e984ed2ae
c <git@actorgrid.net>
2023-03-16 15:28:48 -0600

    Made demo into an interactive three.js viewer.

--- demo/shared/src/main/scala/Demo.scala
+++ demo/shared/src/main/scala/Demo.scala
@@ -5,7 +5,7 @@ import shape.*
  * Created by clifton on 1/9/17.
  */
 
-object Demo extends XApp(DivConsole(style = "padding: 8px; overflow: scroll;")) {
+object Demo extends XApp(DivConsole(id = "console", style = "width:100%; height:100px; padding: 8px; overflow: scroll;")) {
 
   val allDemos: Array[Demonstration] = Array[Demonstration](
     Cube,
@@ -16,7 +16,7 @@ object Demo extends XApp(DivConsole(style = "padding: 8px; overflow: scroll;")) 
     Plane
   )
   def main(args: Array[String]): Unit = {
-    for (d <- allDemos) d.demonstrate
+    //for (d <- allDemos) d.demonstrate
     
     //ai.dragonfly.mesh.io.OBJ.fromMesh(Cube.)
   }
